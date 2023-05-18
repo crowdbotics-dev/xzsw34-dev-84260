969f20b5dcef95dd7df951dec3d5c7f20ae9a23d
Crowdbotics <engineering@crowdbotics.com>
2023-05-18 05:55:42 +0000

    Initial commit.

---
+++ backend/xzsw34_dev_84260/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for xzsw34_dev_84260 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xzsw34_dev_84260.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xzsw34-dev-84260",
+  url: "https://xzsw34-dev-84260.botics.co",
+  api: "https://xzsw34-dev-84260.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
